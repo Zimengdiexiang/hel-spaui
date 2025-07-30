82ec9c1c3ec297e0025d7b8ac7171cae21e757ca
liliannanli <liliannanli@tencent.com>
2025-07-30 21:13:14 +0800

    feat: up

--- config/appInfo.js
+++ config/appInfo.js
@@ -1,6 +1,6 @@
 const helDevUtils = require('hel-dev-utils');
 const pkg = require('../package.json');
 
-const appInfo = helDevUtils.createReactSubApp(pkg, { npmCdnType: 'unpkg' });
+const appInfo = helDevUtils.createReactSubApp(pkg);
 
 module.exports = appInfo;
