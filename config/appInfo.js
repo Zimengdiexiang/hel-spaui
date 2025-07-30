ecb5caf6c1af12fde0270f60203a9383e3dfa41d
liliannanli <liliannanli@tencent.com>
2025-07-30 21:35:36 +0800

    feat: up

--- config/appInfo.js
+++ config/appInfo.js
@@ -1,6 +1,6 @@
 const helDevUtils = require('hel-dev-utils');
 const pkg = require('../package.json');
 
-const appInfo = helDevUtils.createReactSubApp(pkg);
+const appInfo = helDevUtils.createReactSubApp(pkg,{ homePage: 'https://mirrors.tencent.com/npm' });
 
 module.exports = appInfo;
